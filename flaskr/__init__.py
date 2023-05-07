58873e91e7568340ccf6836806d4c8fc7c742419
Fraser Parlane <mail@fraserparlane.com>
2023-05-07 00:01:15 -0700

    Basic search

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,6 @@
 """The main flask app."""
 from flask import Flask, request, render_template
+import requests
 
 
 def create_app(test_config=None):
@@ -9,12 +10,21 @@ def create_app(test_config=None):
 
     @app.route('/')
     def index():
+        """The home page."""
         return render_template('home.html')
 
     @app.route('/search', methods=['GET'])
     def search():
         """Search for a species."""
-        print('a')
+
+        # Get the search string
+        search_str = request.args['search']
+        print(search_str)
+        url = 'https://api.gbif.org/v1/species/search'
+        result = requests.get(url, params={'q': search_str})
+        print(result.json())
+        return render_template('home.html')
+
 
     return app
 
